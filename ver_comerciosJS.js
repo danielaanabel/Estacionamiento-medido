443750d43ccac4402ced5fc00f2d68c152363605
danielaanabel <danielaanabel2011@gmail.com>
2021-06-09 09:55:37 -0300

    agregue popups

--- ver_comerciosJS.js
+++ ver_comerciosJS.js
@@ -12,19 +12,19 @@ function ejecutarMapa() {
    
 
     var comercio1=L.marker([-34.488436, -58.725576]);
-    comercio1.addTo(map).bindPopup("<b>Kiosco Luz!</b><br>abierto de 9 a 19hrs.");
+    comercio1.addTo(map).bindPopup("<b>Kiosco Luz!</b><br>abierto de 9 a 19hrs.<br> Hipólito Bouchard 1366");
     var comercio2=L.marker([-34.497772, -58.728851]);
-    comercio2.addTo(map);
+    comercio2.addTo(map).bindPopup("<b>Almacen Sarita!</b><br>abierto de 8 a 21hrs.<br> Paso de los Patos & Pasco");
     var comercio3=L.marker([-34.492622, -58.730610]);
-    comercio3.addTo(map);
+    comercio3.addTo(map).bindPopup("<b>Locutorio Dandroid!</b><br>abierto de 10 a 18hrs.<br> Padre Stoppler 917");
     var comercio4=L.marker([-34.493962, -58.720538]);
-    comercio4.addTo(map);
+    comercio4.addTo(map).bindPopup("<b>Kiosco Ave Fénix!</b><br>abierto de 9 a 19hrs.<br> El Callao 750");
     var comercio5=L.marker([-34.487477, -58.720972]);
-    comercio5.addTo(map);
+    comercio5.addTo(map).bindPopup("<b>Libreria Koko!</b><br>abierto de 11 a 17hrs.<br> Juan Francisco Seguí 1214");
     var comercio6=L.marker([-34.487001, -58.732645]);
-    comercio6.addTo(map);
+    comercio6.addTo(map).bindPopup("<b>Kiosco Latino!</b><br>abierto de 8 a 22hrs.<br> Luis Vernet 1139");
     var comercio7=L.marker([-34.496992, -58.718191]);
-    comercio7.addTo(map);
+    comercio7.addTo(map).bindPopup("<b>Celulares Nirvana!</b><br>abierto de 9 a 19hrs.<br> El Callao 525");
 
      var cluster = L.markerClusterGroup();
     cluster.addLayers([
